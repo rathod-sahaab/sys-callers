b33f46c1d9afd19aee1d4648ab1e80947976396e
Abhay <abhayonlyone@gmail.com>
2021-03-30 14:02:05 +0530

    Added utils to pring errors and success

---
+++ sysls/src/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+namespace sysls {
+void print_error(const std::string &msg);
+void print_success(const std::string &msg);
+} // namespace sysls
+
+#endif // UTILS_H
